afcef32f87e5dd00f70b9decf60288f7bc5aad49
mario-mitchell <mario.mitchell8291@gmail.com>
2018-12-21 13:54:56 -0800

    adding steps to lab

--- app.js
+++ app.js
@@ -2,3 +2,12 @@
 var name = "Mario Mitchell";
 //Number of US States
 const numberOfUsStates = 50;
+//Adding 5 and 4
+var add = 5 + 4;
+function sayHello(){
+    alert("Hello World!");
+}
+
+sayHello();
+
+
